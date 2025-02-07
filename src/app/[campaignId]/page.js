a37f752798f2e5164fdca9193889882f746e54bf
SamadSRahman <samadsrahman@gmail.com>
2025-02-07 18:18:43 +0530

    logic modified

--- src/app/[campaignId]/page.js
+++ src/app/[campaignId]/page.js
@@ -1,49 +1,5 @@
 import { notFound } from "next/navigation";
 import { fetchCampaignData } from "../utils";
-import Preview from "@/components/Preview/Preview";
-
-export async function generateMetadata({ params }) {
-  const { campaignId } = params;
-  const { campaignData } = await fetchCampaignData(campaignId);
-
-  if (!campaignData) {
-    return {
-      title: "Campaign Not Found",
-      description: "This campaign does not exist.",
-      openGraph: {
-        title: "Campaign Not Found",
-        description: "No details available for this campaign.",
-      },
-    };
-  }
-
-  return {
-    title: campaignData.title,
-    description: campaignData.description,
-    openGraph: {
-      title: campaignData.title,
-      description: campaignData.description,
-      url: `https://yourwebsite.com/campaign/${campaignId}`,
-      images: [
-        {
-          url: campaignData.image || "https://yourwebsite.com/default-image.jpg",
-          width: 1200,
-          height: 630,
-          alt: campaignData.title,
-        },
-      ],
-      type: "website",
-    },
-    alternates: {
-      canonical: `https://appclip.apple.com/id?p=com.xircular.XplorePromote.Clip`,
-    },
-    other: {
-      "apple-itunes-app": "app-clip-bundle-id=com.xircular.XplorePromote.Clip, app-clip-display=card",
-      "appstore:bundle_id": "com.xircular.xplorecampaign",
-      "appstore:store_id": "com.xircular.xplorecampaign",
-    },
-  };
-}
 
 export default async function CampaignPage({ params }) {
   const { campaignId } = params;
@@ -54,8 +10,15 @@ export default async function CampaignPage({ params }) {
   }
 
   return (
-    <main>
-      <Preview campaignId={campaignId} layouts={layouts} />
-    </main>
+   <>
+     <head>
+      <link rel="icon" href={campaignData.image} sizes="256x256" type="image/x-icon"></link>
+        <title>{campaignData.title}</title>
+        <meta name="description" content={campaignData.description} />
+        <meta property="og:title" content={campaignData.title} />
+        <meta property="og:description" content={campaignData.description} />
+        <meta property="og:image" content={campaignData.image} />
+      </head>
+   </>
   );
 }
